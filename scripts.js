1294dbe876d66c45b8997c22b25c5c412035670e
edjunma <junwenma@buffalo.edu>
2019-02-12 04:47:00 -0500

    Fixed some bugs and errors, added match function
    Fixed some import problems with images but still have a bug/problem with the flip function. Cards can be clicked but aren't flipped.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,36 @@
 const cards = document.querySelectorall('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+// the first click
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+// the second click
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
